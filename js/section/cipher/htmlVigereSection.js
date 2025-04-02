1720e83a86d533cc93f1c264428b7bafbbb13243
Kai <jauregiazcue@gmail.com>
2025-04-02 09:14:28 +0200

    finished vigere cipher

--- js/section/cipher/htmlVigereSection.js
+++ js/section/cipher/htmlVigereSection.js
@@ -13,14 +13,25 @@ class HTMLVigereSection extends HTMLCaesarSection {
         this.key.value = "key";
     }
 
+    setFunction() {
+        this.keyIndex = 0;
+        this.leftInput.value = this.leftInput.value.toLowerCase();
+        let leftArray = this.leftInput.value.split("");
+        let rightString = "";
+        for(let i = 0; i < leftArray.length; i++) {
+            rightString += this.changeCharacter(leftArray[i]);
+        }
+        this.rightInput.value = rightString;
+    }
+
     changeCharacter(character) {
         if(this.key.value == "") return character;
         let keyArray = this.key.value.split("");
         let index = this.characters.indexOf(character);
 
-        let newCharacter = (index + this.characters.indexOf(keyArray[this.keyIndex ])) % this.characters.length;
+        let newCharacter = (index + this.characters.indexOf(keyArray[this.keyIndex]) + 1) % this.characters.length;
         this.keyIndex ++;
-        if(this.keyIndex > keyArray.length) this.keyIndex = 0;
+        if(this.keyIndex >= keyArray.length) this.keyIndex = 0;
 
         return this.characters[newCharacter];
     }
